43e349ff5a070bcd9158bb3a0617ca4adabccc15
dfpiscitelli <damianpiscitelli@gmail.com>
2022-06-15 09:56:49 -0300

    push de mercado liebre

---
+++ source/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const { get } = require("http");
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 2003;
+
+app.listen(2003, () => {  
+    console.log("corriendo on port 2003");
+});
+const pathPublic = path.resolve(__dirname, '../public');
+app.use(express.static(pathPublic));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../views/home.html'));
+})
+
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+})
